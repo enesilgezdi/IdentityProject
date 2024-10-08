5791900de3b2b9cf1aa0fdbc26ef9727ce5d62bf
Enes İlgezdi <enesilgezdi@gmail.com>
2024-10-09 00:01:04 +0300

    EntityFrameWork ,EntitiyFrameWorkSQL, EntityFrameWorkTools, ekleyip verileri çektim

---
+++ IdentityProject.WebApi1/Contexts/MsSqlContext.cs
@@ -0,0 +1,20 @@
+﻿using IdentityProject.WebApi1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityProject.WebApi1.Contexts;
+
+public class MsSqlContext : DbContext
+
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        // Docker kurulu olanlar 
+        //optionsBuilder.UseSqlServer("Server= localhost,1433; Database = Identit_db; User=sa; Password=admin123456789; TrustServerCertificate=true");
+
+        // Localdb
+        //optionsBuilder.UseSqlServer(@"Server= (localdb)\MSSQLLocalDB; Database=Identit_db; Trusted_Connection = true");
+        optionsBuilder.UseSqlServer(@"Server= (localdb)\MSSQLLocalDB; Database=Identity_db; Trusted_Connection = true");
+    }
+
+    public DbSet<User> Users { get; set; }
+}
--- IdentityProject.WebApi1/Controllers/UserController.cs
+++ IdentityProject.WebApi1/Controllers/UserController.cs
@@ -1,135 +1,139 @@
-﻿using IdentityProject.WebApi1.Models;
+﻿using IdentityProject.WebApi1.Contexts;
+using IdentityProject.WebApi1.Models;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using System.Xml.Linq;
 
 namespace IdentityProject.WebApi1.Controllers;
 
+[Route("api/[controller]")]
+[ApiController]
 public class UserController : ControllerBase
 {
-    private static List<User> users = new List<User>()
-    {
-        new User
-        {
-            Id = 1,
-            Email = "seren@gmail.com",
-            Password = "deneme123",
-            Phone = "05654123687",
-            UserName = "qubitfaruk"
-        },
-        new User
-        {
-            Id = 2,
-            Email = "muhammed_kus@gmail.com",
-            Password = "deneme_123",
-            Phone = "05412569874",
-            UserName = "cıstakcıstak"
-        },
-           new User
-        {
-            Id = 3,
-            Email = "safa_turizm@gmail.com",
-            Password = "sarımsak37",
-            Phone = "05412569884",
-            UserName = "gastamonulu_baba"
-        }
-    };
-
-    [HttpGet("getall")]
-    public List<User> GetUsers()
-    {
-        return users;
-    }
-
-    [HttpGet("getbyid")]
-    // http://localhost:5242/api/getbyid/3
-    public IActionResult GetById(int id)
-    {
-
-
-        User user = users.FirstOrDefault(x => x.Id == id);
-        if (user is null)
-        {
-            return NotFound("Aradığınız kullanıcı bulunamadı.");
-        }
-
-        return Ok(user);
-
-
-    }
-
-    [HttpPost("add")]
-    public IActionResult Add([FromBody] User user)
-    {
-        User? getUserByEmail = users.SingleOrDefault(x => x.Email == user.Email);
-        if (getUserByEmail is not null)
-        {
-            string message = $"Aynı email e ait kullanıcı mevcuttur: {user.Email}";
-            return BadRequest(message);
-        }
-
-        users.Add(user);
-        return Ok("Kullanıcı eklendi.");
-    }
-
-
-    [HttpDelete("delete")]
-    public IActionResult Delete(string email)
-    {
-        User? user = users.SingleOrDefault(u => u.Email == email);
-        if (user is null)
-        {
-            return NotFound("Aradığınız email e göre kullanıcı bulunamadı.");
-        }
-
-        users.Remove(user);
-        return Ok(user);
-    }
-
-    [HttpPut("Update")]
-    public IActionResult Update([FromQuery] int id, [FromBody] User updated)
-    {
-        User user = users.FirstOrDefault(x => x.Id == id);
-        if (user is null)
-        {
-            return NotFound("Aradığınız kullanıcı bulunamadı.");
-        }
-
-        int index = users.IndexOf(user);
-        users.Remove(user);
-        users.Insert(index, updated);
-
-        return Ok("Güncellendi.");
-
-    }
-
-    //[HttpPost("add")]
-    //public IActionResult Add(User user)
+    MsSqlContext msSqlContext = new MsSqlContext();
+    //private static List<User> users = new List<User>()
     //{
-    //    // insert into Users(...) values (...)
-    //    msSqlContext.Users.Add(user);
-    //    msSqlContext.SaveChanges();
-
-    //    return Ok(user);
-    //}
-    //// IEntityTypeConfigurationBuilder
-
+    //    new User
+    //    {
+    //        Id = 1,
+    //        Email = "seren@gmail.com",
+    //        Password = "deneme123",
+    //        Phone = "05654123687",
+    //        UserName = "qubitfaruk"
+    //    },
+    //    new User
+    //    {
+    //        Id = 2,
+    //        Email = "muhammed_kus@gmail.com",
+    //        Password = "deneme_123",
+    //        Phone = "05412569874",
+    //        UserName = "cıstakcıstak"
+    //    },
+    //       new User
+    //    {
+    //        Id = 3,
+    //        Email = "safa_turizm@gmail.com",
+    //        Password = "sarımsak37",
+    //        Phone = "05412569884",
+    //        UserName = "gastamonulu_baba"
+    //    }
+    //};
 
     //[HttpGet("getall")]
-    //public IActionResult GetAll()
+    //public List<User> GetUsers()
     //{
-    //    // select * from users
-    //    List<User> users = msSqlContext.Users.ToList();
-    //    return Ok(users);
+    //    return users;
     //}
 
     //[HttpGet("getbyid")]
+    //// http://localhost:5242/api/getbyid/3
     //public IActionResult GetById(int id)
     //{
-    //    // select * from users where id = (id)
-    //    User user = msSqlContext.Users.Find(id);
+
+
+    //    User user = users.FirstOrDefault(x => x.Id == id);
+    //    if (user is null)
+    //    {
+    //        return NotFound("Aradığınız kullanıcı bulunamadı.");
+    //    }
 
     //    return Ok(user);
 
+
     //}
+
+    //[HttpPost("add")]
+    //public IActionResult Add([FromBody] User user)
+    //{
+    //    User? getUserByEmail = users.SingleOrDefault(x => x.Email == user.Email);
+    //    if (getUserByEmail is not null)
+    //    {
+    //        string message = $"Aynı email e ait kullanıcı mevcuttur: {user.Email}";
+    //        return BadRequest(message);
+    //    }
+
+    //    users.Add(user);
+    //    return Ok("Kullanıcı eklendi.");
+    //}
+
+
+    //[HttpDelete("delete")]
+    //public IActionResult Delete(string email)
+    //{
+    //    User? user = users.SingleOrDefault(u => u.Email == email);
+    //    if (user is null)
+    //    {
+    //        return NotFound("Aradığınız email e göre kullanıcı bulunamadı.");
+    //    }
+
+    //    users.Remove(user);
+    //    return Ok(user);
+    //}
+
+    //[HttpPut("Update")]
+    //public IActionResult Update([FromQuery] int id, [FromBody] User updated)
+    //{
+    //    User user = users.FirstOrDefault(x => x.Id == id);
+    //    if (user is null)
+    //    {
+    //        return NotFound("Aradığınız kullanıcı bulunamadı.");
+    //    }
+
+    //    int index = users.IndexOf(user);
+    //    users.Remove(user);
+    //    users.Insert(index, updated);
+
+    //    return Ok("Güncellendi.");
+
+    //}
+
+    [HttpPost("add")]
+    public IActionResult Add(User user)
+    {
+        // insert into Users(...) values (...)
+        msSqlContext.Users.Add(user);
+        msSqlContext.SaveChanges();
+
+        return Ok(user);
+    }
+    // IEntityTypeConfigurationBuilder
+
+
+    [HttpGet("getall")]
+    public IActionResult GetAll()
+    {
+        // select * from users
+        List<User> users = msSqlContext.Users.ToList();
+        return Ok(users);
+    }
+
+    [HttpGet("getbyid")]
+    public IActionResult GetById(int id)
+    {
+        // select * from users where id = (id)
+        User user = msSqlContext.Users.Find(id);
+
+        return Ok(user);
+
+    }
 }
