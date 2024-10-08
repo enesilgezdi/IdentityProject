3a6c4faf8e8a2e4633c0f5d0d61c701ee3fd6b45
Enes İlgezdi <enesilgezdi@gmail.com>
2024-10-08 22:58:39 +0300

    controllerde list olusturup api den listeyi cektim

--- IdentityProject.WebApi1/Controllers/UserController.cs
+++ IdentityProject.WebApi1/Controllers/UserController.cs
@@ -103,4 +103,33 @@ public class UserController : ControllerBase
 
     }
 
+    //[HttpPost("add")]
+    //public IActionResult Add(User user)
+    //{
+    //    // insert into Users(...) values (...)
+    //    msSqlContext.Users.Add(user);
+    //    msSqlContext.SaveChanges();
+
+    //    return Ok(user);
+    //}
+    //// IEntityTypeConfigurationBuilder
+
+
+    //[HttpGet("getall")]
+    //public IActionResult GetAll()
+    //{
+    //    // select * from users
+    //    List<User> users = msSqlContext.Users.ToList();
+    //    return Ok(users);
+    //}
+
+    //[HttpGet("getbyid")]
+    //public IActionResult GetById(int id)
+    //{
+    //    // select * from users where id = (id)
+    //    User user = msSqlContext.Users.Find(id);
+
+    //    return Ok(user);
+
+    //}
 }
